e90f42dc7e32291bd7d9d3fdf4a793221388d294
Gabes <gabnology+github@gmail.com>
2022-10-11 11:37:50 +0000

    progress

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ const app = express();
 
 app.use(express.static("public"));
 
-const PORT = process.env.PORT || 3001
+let lastGameId = 0
+const games = []
+const matchmakingPlayers = [] // waiting players
+
+function createGame(player1, player2){
+    games.push({
+        player1: player1,
+        player2: player2,
+        id: lastGameId + 1
+    });
+    lastGameId += 1
+}
+
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
