c0c985d2e9edf9cafbed2345e2d2a4f5eceb3ada
Gabes <gabnology+github@gmail.com>
2022-10-14 08:33:12 +0000

    Import home.js

---
+++ public/home.js
@@ -0,0 +1,12 @@
+const unameinput = document.getElementById("username")
+
+function startGame(){
+    //button pressed
+    //check for username input
+    let username = unameinput.value
+    if(username === ""){
+        alert("Enter a username");
+        return
+    }
+    
+}
